beb956412b8f2151e7487ca70f9ce8b28f0f936d
issarompion <irompion@yahoo.fr>
2020-05-11 14:13:06 +0200

    Auto-generated commit

--- README.md
+++
@@ -1,7 +0,0 @@
-# pokedemo
-
-Pokédex web application with angular
-
-## Run the project
-
-`npm install`, `npm run start` and go to `localhost:4200/`.
--- src/app/filter-pokemon--pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterPokemonPipePipe } from './filter-pokemon--pipe.pipe';
-
-describe('FilterPokemonPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterPokemonPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
